7e1b616927ff06a6d3fe9f291d40fb260b15d938
lvyong <lvyong1985@hotmail.com>
2021-06-02 13:17:37 +0800

    first commit

---
+++ src/main/java/io/github/gobars/trial/javatrialsandbox/JavaTrialSandboxApplication.java
@@ -0,0 +1,13 @@
+package io.github.gobars.trial.javatrialsandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTrialSandboxApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JavaTrialSandboxApplication.class, args);
+  }
+
+}
